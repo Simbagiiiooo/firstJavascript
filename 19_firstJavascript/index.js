2e963543ab85986e712ddacb92f1a63aea421c38
Simbagiiiooo <67731418+Simbagiiiooo@users.noreply.github.com>
2020-12-28 10:10:42 +0100

    Add files via upload

---
+++ 19_firstJavascript/index.js
@@ -0,0 +1,32 @@
+console.log("Hello Winc Academy")
+
+let firstName = 'Ralf';
+let lastName = 'van Eck';
+console.log(firstName, lastName);
+
+// je kan geen variable maken met een reserved keyword.
+
+let sum = 4 + 4;
+console.log(sum);
+
+let sumString = '4' + '4';
+console.log(sumString);
+// Je kan 2 strings niet bij elkaar optellen, deze kpomen achter elkaar.
+
+let sumAddition = 158 + 142;
+console.log(sumAddition);
+let sumSubstraction = 100 - 46;
+console.log(sumSubstraction);
+let sumMultiplication = 4 * 4;
+console.log(sumMultiplication);
+let sumDivision = 100 / 4;
+console.log(sumDivision);
+let sumModulus = 5 % 2;
+console.log(sumModulus);
+
+let age = 47;
+// antwoord in de console met typeof age = "number"
+
+let ageWriten = 'zevenenveertig';
+// antwoord in de console met typeof ageWriten = "string"
+
